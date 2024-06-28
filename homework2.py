57aeca6ffeb2167cd2c4ca36eabe4d34da3b32fd
ErofeevVV <102551371+ErofeevVV@users.noreply.github.com>
2024-06-28 22:06:19 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_completed = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / homework_completed
+print('Курс:', course_name, ',', 'всего задач:', homework_completed, ',', 'среднее время выполнения', time_per_task)
