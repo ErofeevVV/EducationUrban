5b13139acc65534c4e937b48fe1a114b705a6ff2
ErofeevVV <102551371+ErofeevVV@users.noreply.github.com>
2024-06-30 21:29:20 +0300

    Delete homework4.py

--- homework4.py
+++
@@ -1,7 +0,0 @@
-my_string = input('Введите любой текст: ')
-print(my_string)
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1])
