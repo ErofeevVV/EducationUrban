c42bfeefe3f95a0e05e11c978f359e32bb2f9230
ErofeevVV <102551371+ErofeevVV@users.noreply.github.com>
2024-07-06 17:01:12 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,24 @@
+value = int(input('Введите число от 3 до 20: '))
+password = []
+
+def getting_a_password(value):
+    for i in range(1, 21):
+        if i <= value:
+            for j in range(1, 21):
+                if j <= value:
+                    if i >= j:
+                        continue
+                    if value % (i + j) == 0:
+                        password.extend([i, j])
+                else: break
+        else: break
+
+    print('Пароль: ', *password)
+
+if 3 <= value <= 20:
+    getting_a_password(value)
+else:
+    print('Введено некоректное значение')
+
+
+
