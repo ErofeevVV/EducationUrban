ece004a3d02d5d05b8769e236b4c7192f1bf0990
ErofeevVV <102551371+ErofeevVV@users.noreply.github.com>
2024-10-30 21:44:20 +0300

    Add files via upload

---
+++ module_10_2.py
@@ -0,0 +1,35 @@
+import threading
+import time
+
+
+class Knight(threading.Thread):
+    def __init__(self, name, power, number_of_enemies):
+        super().__init__()
+        self.name = name
+        self.power = power
+        self.number_of_enemies = number_of_enemies
+        self.days_of_battle = 0
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        while self.number_of_enemies > 0:
+            self.days_of_battle += 1
+            self.number_of_enemies -= self.power
+            if self.number_of_enemies <= 0:
+                break
+            print(f'{self.name} сражается {self.days_of_battle} день(дня)..., '
+                  f'осталось {self.number_of_enemies} воинов.')
+            time.sleep(3)
+        print(f'{self.name} одержал победу спустя {self.days_of_battle} дней(дня)!')
+
+
+knight_1 = Knight('Artur', 10, 50)
+knight_2 = Knight('Richard', 25, 100)
+
+knight_1.start()
+knight_2.start()
+
+knight_1.join()
+knight_2.join()
+
+print('Все битвы закончились!')
